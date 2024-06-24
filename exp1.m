54a454083f780061877e2171abce67af8d947014
lvkexin <Kelen_Lv@sjtu.edu.cn>
2024-06-24 17:11:34 +0800

    Update exp1.m

--- exp1.m
+++ exp1.m
@@ -2,26 +2,22 @@ clc
 clear all
 close all
 warning off
-%% graph settings
+%% graph settings: circle topology
 N=5;
-%  G=gen_graph(N);
-  G=[0     1     0     0     1;
+G=[0     1     0     0     1;
      1     0     1     0     0;
      0     1     0     1     0;
      0     0     1     0     1;
-     1     0     0     1     0];%circle
-% G=[0 1 0 1;1 0 1 0 ;0 1 0 1;1 0 1 0];%N=4 circle
-%  G_r=G(:);
+     1     0     0     1     0];
 G_=triu(G);
- index=find(G_(:));
+index=find(G_(:));
 for i=1:N
    n(:,i)=sum(G(:,i));% number of neighbor
 end
+
 %% sample distributed
 d=100;
 load('data.mat');
-% chol(A);
-% chol(B);
 sumA=A/100*N;
 sumB=B/100*N;
 clear A B
@@ -31,19 +27,16 @@ clear A B
 global VV
 VV=V(:,1)/sqrt(norm(V(:,1)'*sumB*V(:,1)));% constraint norm(VV(:,1)'*sumA*VV(:,1)),norm(VV(:,1)'*sumB*VV(:,1))
 F_true=-norm(VV(:,1)'*sumA*VV(:,1));
-% VV=V/sqrt(V(1,:)*sumB*V(1,:)');% constraint norm(VV(:,1)'*sumA*VV(:,1)),norm(VV(:,1)'*sumB*VV(:,1))
-% F_true=-norm(VV(1,:)*sumA*VV(1,:)');
-%% parameter initialization
-rho1=100;%Íâ50
-rho2=100;%ÄÚ100
 
+%% parameter initialization
+rho1=100;
+rho2=100;
 w_init=randn(d,1);
 w_init=w_init/sqrt(w_init'*sumB*w_init);%must
-% load('wronginit.mat');
 l_init=zeros(d,1);
-% z_init=w_init+l_init/rho2;
 E_list=[];
 L_list=[];
+
 %% local data preparation
 w_b=0;
 for i=1:N
@@ -66,6 +59,7 @@ while 1
     w_b_old=w_b;
     iter=iter+1;
     flag=zeros(N,1);
+    
     %% z update in the FC
     w_m=0;
     l_m=0;
@@ -77,34 +71,29 @@ while 1
     l_m=l_m/N;
     z=w_m+l_m/rho1;
     fprintf('outerL after z: %0.5f\n',outerL(N,w,A,l,z,rho1));
+    
     %% w-update
     while 1
          r=randperm(size(index,1));
-         s_rp=r(1:size(r,2)/2);%    
-%          resz=0;
-%          zm=0;
+         s_rp=r(1:size(r,2)/2);
          for k=1:size(s_rp,2)%3N
              ii=floor((index(s_rp(k))-1)/N)+1;
              jj=index(s_rp(k))-(ii-1)*N;
              sumc=cal_globalc(c,N);%(k)
              fprintf('!! Node %d and Node %d are updating!\n',ii,jj);
-%              [w(:,:,ii),w(:,:,jj),c(:,ii),c(:,jj),zz(:,ii),zz(:,jj)]=inner_loop(ii,jj,A,B,rho1,d,sumc,w(:,:,ii),w(:,:,jj));
              [w(:,:,ii),w(:,:,jj),c(:,ii),c(:,jj)]=inner_loop(ii,jj,A,B,rho1,d,sumc,w(:,:,ii),w(:,:,jj),l(:,:,ii),l(:,:,jj),z,rho2);
-%              fprintf('!! Node %d and Node %d are updating!\n',ii(k),jj(k));
-%              [w(:,:,ii(k)),w(:,:,jj(k)),c(:,ii(k)),c(:,jj(k))]=inner_loop(ii(k),jj(k),A,B,rho1,d,sumc,w(:,:,ii(k)),w(:,:,jj(k)));
              flag(ii)=1;
              flag(jj)=1;
              [w(:,:,ii),w(:,:,jj)]=check_allign(VV,w(:,:,ii),w(:,:,jj));
          end
          if isempty(find(~flag))%&& resz<1e-3
                 break;
-%          else
-%                  flag=zeros(N,1);
          end
     end
     L=outerL(N,w,A,l,z,rho1);
     fprintf('outerL after w_i: %0.5f\n',L);
     L_list=[L_list L];
+    
     %% lambda update
     for i=1:N
         temp=l(:,:,i)+rho2*(w(:,:,i)-z);
@@ -113,6 +102,7 @@ while 1
         end
     end
     fprintf('outerL after l_i: %0.5f\n',outerL(N,w,A,l,z,rho1));
+    
     %% stop criteria
     w_b=0;
     for i=1:N
@@ -131,13 +121,13 @@ while 1
             break;
     else
             fprintf('#complete outer iter=%d, res1=%0.5f, res2=%0.5f\n',iter,res1,res2);% 
-%             fprintf('\n')
     end
     sin(subspace(VV,w_b))
     E_list=[E_list norm(VV-w_b)];
    
 end
-%%
+
+%% plotting figures
 figure; yyaxis left; 
 plot(E_list,'LineWidth',1);
 title('Convergence performance of Alg.1','interpreter','latex', 'FontSize', 18);
@@ -156,12 +146,10 @@ function [wi,wj]=check_allign(w_b,wi,wj)
         wj=-wj;
     end
 end
-function  [L]=outerL(N,w,A,l,z,rho2)%,F
+function  [L]=outerL(N,w,A,l,z,rho2)
     L=0;
-%     F=0;
     for i=1:N
         L=L-w(:,:,i)'*A(:,:,i)*w(:,:,i)+l(:,:,i)'*(w(:,:,i)-z)+rho2/2*(norm(w(:,:,i)-z)^2);
-%         F=F-w(:,:,i)'*A(:,:,i)*w(:,:,i)
     end
 end
 function sumc=cal_globalc(c,N)
@@ -213,7 +201,7 @@ function [wi,wj,ci,cj]=inner_loop(i,j,A,B,rho1,d,sumc,wi,wj,li,lj,z,rho2)
         wj_old=wj;
 %         cj+ci-c
 %         flag=0;
-%         wi=inv_ill(2*(a*Bi-Ai+rho1/2*eye(d)))*(rho1*z-li);   %pinv
+%         wi=inv_ill(2*(a*Bi-Ai+rho1/2*eye(d)))*(rho1*z-li);   %pin
         wi=w_GD(wi,Ai,Bi,rho1,rho2,z,a,li,cj,c);
 %         fprintf('L after wi: %0.5f\n',innerLGD(wi,wj,Ai,Aj,Bi,Bj,a,li,lj,rho1,c,z,rho2));   
 %         fprintf('L after wi: %0.5f\n',innerL(wi,wj,Ai,Aj,Bi,Bj,a,li,lj,rho1,c,z)); 
